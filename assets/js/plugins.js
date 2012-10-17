b898ffef02742fb056e9aa49aab2e17a7a16b245
bradstrong <bradcstrong@gmail.com>
2012-10-17 18:01:07 -0400

    Updated recent to include source information

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -9,69 +9,17 @@
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
-//Brad's update.html
-
-	// <!-- Included JS Files -->
-	// <script src="assets/js/libs/jquery.min.js"></script>
-	// <script src="assets/js/foundation/jquery.reveal.js"></script>
-	// <script src="assets/js/foundation/jquery.orbit-1.4.0.js"></script>
-	// <script src="assets/js/foundation/jquery.customforms.js"></script>
-	// <script src="assets/js/foundation/jquery.placeholder.min.js"></script>
-	// <script src="assets/js/foundation/jquery.tooltips.js"></script>
-
-	// <!-- <script src="javascripts/app.js"></script> -->
-	// 	<script src="assets/js/app.js"></script>
-	//   <script src="assets/js/foundation/jquery.offcanvas.js"></script>
-	// 	<script src="assets/js/mustache.js"></script>
-
-
-		//Foundation Index
-
-	// 		<!-- Included JS Files (Uncompressed) -->
-	// <script src="javascripts/foundation/jquery.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.accordion.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.alerts.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.buttons.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.forms.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.mediaQueryToggle.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.navigation.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.orbit.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.reveal.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.tabs.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.foundation.tooltips.js"></script>
-	
-	// <script src="javascripts/foundation/jquery.placeholder.js"></script>
-	
-	
-
-// (function ($) {  
-
-// 	  $.getScript("foundation/jquery.foundation.accordion.js");
-// 	  $.getScript("foundation/jquery.foundation.alerts.js");
-// 	  $.getScript("foundation/jquery.foundation.buttons.js");
-// 	  $.getScript("foundation/jquery.foundation.forms.js");
-// 	  $.getScript("foundation/jquery.foundation.mediaQueryToggle.");
-// 	  $.getScript("foundation/jquery.foundation.navigation.js");
-// 	  $.getScript("foundation/jquery.foundation.orbit.js");
-// 	  $.getScript("foundation/jquery.foundation.reveal.js");
-// 	  $.getScript("foundation/jquery.foundation.tabs.js");
-// 	  $.getScript("foundation/jquery.foundation.tooltips.js");
-// 	  $.getScript("foundation/jquery.placeholder.js");
-
-// 		$.getScript("foundation/jquery.offcanvas.js");
-// 		$.getScript("mustache.js");
-  
-// })(jQuery);
-(function ($) { 
-	compileMustacheTemplates();
-})(jQuery);
+// @codekit-append "js/foundation/jquery.js"
+// @codekit-append "js/foundation/jquery.foundation.accordion.js"
+// @codekit-append "js/foundation/jquery.foundation.alerts.js"
+// @codekit-append "js/foundation/jquery.foundation.buttons.js"
+// @codekit-append "js/foundation/jquery.foundation.forms.js"
+// @codekit-append "js/foundation/jquery.foundation.mediaQueryToggle.js"
+// @codekit-append "js/foundation/jquery.foundation.navigation.js"
+// @codekit-append "js/foundation/jquery.foundation.orbit.js"
+// @codekit-append "js/foundation/jquery.foundation.reveal.js"
+// @codekit-append "js/foundation/jquery.foundation.tabs.js"
+// @codekit-append "js/foundation/jquery.foundation.tooltips.js"
+// @codekit-append "js/foundation/jquery.foundation.topbar.js"
+// @codekit-append "js/foundation/jquery.placeholder.js"
+// @codekit-append "js/foundation/jquery.offcanvas.js"
