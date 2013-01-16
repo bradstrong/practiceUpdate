e846e67cf8d1186f35172466c9a09e026a454aa1
bradstrong <bradcstrong@gmail.com>
2013-01-15 20:05:01 -0500

    - the latest version of the styles im working with uses foundation 3.2.4 (upgraded from 3.2.2) - this fixes several bugs, such as the non-closing accordions, that Nix built code to work around - I've added functions to app.js, but I want to make sure that this should be accomplished via editing templates. - I've corrected markup for registration/preferences and I want to make sure you know what's changed

--- assets/js/foundation/app.js
+++ assets/js/foundation/app.js
@@ -1,4 +1,45 @@
- ;(function ($, window, undefined) {
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        // At load, if user hasn't scrolled more than 20px or so...
+  			if( $(window).scrollTop() < 20 ) {
+          window.scrollTo(0, 1);
+        }
+      }, 0);
+    });
+  }
+
+})(jQuery, this);
+
 /*--
 //
 //PracticeUpdate Scripts
@@ -20,7 +61,7 @@
   console.log('toggle menu');
   $('ul.user-settings-menu').addClass('is-collapsed');
   $('ul.user-settings-menu li:first').addClass('active');
-  $('li.active a').append('<span class="active-item-indicator"><i class="icon-angle-right"></i></span>');
+  $('li.active > a, dd.active > a').append('<span class="active-item-indicator"><i class="icon-chevron-right"></i></span>');
   $('.edit-user-settings').click(function (){
     $('.user-settings-menu').toggleClass('is-collapsed is-expanded');
   });
@@ -49,17 +90,17 @@
         $('.page-header').html(html);
         //Disable "Learn" during beta phase -- Remove once Learn/CME goes live
         //$('.top-bar a.learn').attr('data-reveal-id', 'modal-feature-disabled');
-		
-		var b = $('body');
-		if(b.hasClass('page-update')) {
-			$('ul#main-nav .update').parent('li').addClass('active');
-		}
-		if(b.hasClass('page-explore')) {
-			$('ul#main-nav .explore').parent('li').addClass('active');
-		}
-		if(b.hasClass('page-learn')) {
-			$('ul#main-nav .learn').parent('li').addClass('active');
-		}
+    
+    var b = $('body');
+    if(b.hasClass('page-update')) {
+      $('ul#main-nav .update').parent('li').addClass('active');
+    }
+    if(b.hasClass('page-explore')) {
+      $('ul#main-nav .explore').parent('li').addClass('active');
+    }
+    if(b.hasClass('page-learn')) {
+      $('ul#main-nav .learn').parent('li').addClass('active');
+    }
       });
     });
     /* Mustache - homepage footer template */
@@ -77,61 +118,16 @@
       var html = Mustache.to_html(template);
       $('body').append(html);
     });
-	
-	$.get('assets/mustache/modals-forgot-password.mustache', function(template) {
+  
+  $.get('assets/mustache/modals-forgot-password.mustache', function(template) {
       var html = Mustache.to_html(template);
       $('body').append(html);
     });
 
     })();
-/*--
-//
-//Begin Original Foundation Scripts
-//
---*/
-  'use strict';
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
-
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
-    $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
-    });
-  };
-/*--
-//
-//End Original Foundation Scripts
-//
- --*/
 
   $(document).ready(function() {
     $('.disabled a').click(function(e) {
       e.preventDefault();
     });
   });
-  
-})(jQuery, this);
