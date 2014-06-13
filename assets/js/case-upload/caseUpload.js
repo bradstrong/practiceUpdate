68a509cb32347254774b9402058157276a619a3e
Liccy Fuentes <liccyfuentes@gmail.com>
2014-06-13 14:23:21 -0400

    adding date to comment, updating text, error msg for submit file

--- assets/js/case-upload/caseUpload.js
+++ assets/js/case-upload/caseUpload.js
@@ -8,6 +8,31 @@ PU.caseUpload = (function ($) {
 						filepath = document.upload.file.value,
 						thefile  = myFSO.getFile(filepath),
 						size     = thefile.size;
+
+				if (size > 2621440) {
+					$.growl('Filesize must be 2.5MB or below', {
+						onGrowlClosed: function() {
+								$('.j-file-upload-name').val('');
+								$('.j-submit-file input').attr("disabled", true);
+						},	
+						ele: "#submitCase",
+						position: {
+								from: "top",
+								align: "center"
+						},
+						type: 'danger',
+						template: {
+								icon_type: 'class',
+								container: '<div class="col-xs-10 col-sm-10 col-md-6 alert">',
+								dismiss: '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>',
+								title: '<strong>',
+								title_divider: '',
+								message: ''
+						}
+					});
+				} else {
+					$('.j-submit-file input').attr("disabled", false);
+				}
 				
 				alert(size + " bytes");
 			}
@@ -24,8 +49,6 @@ PU.caseUpload = (function ($) {
 			$(document).on('keyup', '.j-textarea-field', function(e) {
 				var $elm = $('.j-submit-file input');
 				
-				//this.rows = (this.value.split("\n").length||1);
-
 				if (e.keyCode == 13 && e.shiftKey)
 				 e.stopPropagation();
 				
@@ -60,6 +83,7 @@ PU.caseUpload = (function ($) {
 												$('.j-file-upload-name').val('');
 												$('.j-submit-file input').attr("disabled", true);
 										},	
+										width: "400px",
 										ele: "#submitCase",
 										position: {
 												from: "top",
@@ -68,7 +92,7 @@ PU.caseUpload = (function ($) {
 										type: 'danger',
 										template: {
 												icon_type: 'class',
-												container: '<div class="col-xs-10 col-sm-10 col-md-6 alert">',
+												container: '<div class="col-xs-10 text-center col-sm-10 col-md-6 alert">',
 												dismiss: '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>',
 												title: '<strong>',
 												title_divider: '',
@@ -76,7 +100,7 @@ PU.caseUpload = (function ($) {
 										}
 									});
 								} else {
-									$('.j-submit-file input').attr("disabled", false);;
+									$('.j-submit-file input').attr("disabled", false);
 								}
 							}
 					} catch(e) {
@@ -95,10 +119,32 @@ PU.caseUpload = (function ($) {
 			$(".j-form-submit-case").submit(function(e) {
 				e.preventDefault();
 				
-				$.growl('Your case has been summited.', {
+				$.growl('Success! <br /> Thank you, we\'ve received your case and one of our editors will review it shortly. <br /> PracticeUpdate staff may contact you via email regarding questions, editing, and possible publication on http://PracticeUpdate.com', {
 					onGrowlClosed: function() {
 						$('.j-textarea-field, .j-file-upload-name').val('');
+
+						$.growl('Your case could not be submitted, please try again.', {
+							onGrowlClosed: function() {
+								$('.j-textarea-field, .j-file-upload-name').val('');
+							},	
+							width: "400px",
+							ele: "#submitCase",
+							position: {
+									from: "top",
+									align: "center"
+							},
+							type: 'danger',
+							template: {
+									icon_type: 'class',
+									container: '<div class="col-xs-10 text-center success-mgs col-sm-10 col-md-6 alert">',
+									dismiss: '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>',
+									title: '<strong>',
+									title_divider: '',
+									message: ''
+							}
+						});
 					},	
+					width: "400px",
 					ele: "#submitCase",
 					position: {
 							from: "top",
@@ -107,7 +153,7 @@ PU.caseUpload = (function ($) {
 					type: 'success',
 					template: {
 							icon_type: 'class',
-							container: '<div class="col-xs-10 col-sm-10 col-md-6 alert">',
+							container: '<div class="col-xs-10 text-center success-mgs col-sm-10 col-md-6 alert">',
 							dismiss: '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>',
 							title: '<strong>',
 							title_divider: '',
